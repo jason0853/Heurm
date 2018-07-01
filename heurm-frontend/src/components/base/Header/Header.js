b42e93335d641333329d1d98bf2014b097825ec5
Jason <jason0853@gmail.com>
2018-07-01 13:54:53 +0900

    Add mutilple classnames dynamically

--- heurm-frontend/src/components/base/Header/Header.js
+++ heurm-frontend/src/components/base/Header/Header.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import './header.scss';
 
+const test = true ? 'pink' : '';
+
 const Header = ({ children }) => {
-  return <div className="header">Header</div>;
+  return <div className={['header', test].join(' ')}>Header</div>;
 };
 
 export default Header;
